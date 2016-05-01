2ac28848d4199e3846a130cf6e9471bd0a4bb608
Dhole <dhole@openmailbox.org>
2016-05-01 03:34:22 +0200

    Add regex for quoted strings. Add ProtocolInfo struct

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate log;
 extern crate env_logger;
 extern crate regex;
 
-//use std::path::Path;
+use std::path::Path;
 use std::net::{SocketAddr, TcpStream, Shutdown};
 use std::io::{Read, Write};
 //use std::str;
 use std::io::{BufReader, BufRead, BufWriter};
 //use std::option::Option;
+use std::collections::HashMap;
 
 use regex::Regex;
 
@@ -22,6 +23,24 @@ use regex::Regex;
 //    Tcp(SocketAddr),
 //    Unix(Path),
 //}
+
+#[derive(Debug)]
+enum AuthMethod {
+    Null,
+    HashedPassword,
+    Cookie,
+    SafeCookie,
+}
+
+#[derive(Debug)]
+struct ProtocolInfo {
+    protocol_info_ver: u8,
+    tor_ver: String,
+    auth_methods: Vec<AuthMethod>,
+//    cookie_files: Vec<Path>,
+//    auth_methods: Vec<String>,
+    cookie_files: Vec<String>,
+}
 
 struct Connection<T: Read + Write> {
     raw_stream: T,
@@ -88,44 +107,63 @@ impl<T: Read + Write> Controller<T> {
         }
         reply
     }
-    fn protocolinfo(&mut self) {
+    fn protocolinfo(&mut self) -> ProtocolInfo {
         let reply = self.raw_cmd("PROTOCOLINFO");
+        // regex for QuotedString = (\\.|[^\"])*
         let re_protocolinfo = 
             Regex::new("^250-PROTOCOLINFO (?P<version>[0-9]+)$")
             .unwrap();
         let re_tor_version = 
-            Regex::new("^250-VERSION Tor=\"(?P<tor_version>[^\"]*)\"(?P<opt_arguments>.*)$")
+            Regex::new("^250-VERSION Tor=\"(?P<tor_version>(\\.|[^\"])*)\"(?P<opt_arguments>.*)$")
             .unwrap();
         let re_auth =
             Regex::new("^250-AUTH METHODS=(?P<auth_methods>[A-Z,]+)[  ]*(?P<maybe_cookie_files>.*)$")
             .unwrap();
-        let re_cookiefile =
-            Regex::new("COOKIEFILE=\"(?P<cookie_file>[^\"]*)\"")
+        let re_cookie_file =
+            Regex::new("COOKIEFILE=\"(?P<cookie_file>(\\.|[^\"])*)\"")
             .unwrap();
         let prot_inf = re_protocolinfo.captures(reply[0].as_str()).unwrap();
-        let version = prot_inf.name("version").unwrap();
+        let version = prot_inf.name("version").unwrap().parse::<u8>().unwrap();
+
+        let mut tor_version = String::new();
+        let mut cookie_files = Vec::new();
+        let mut auth_methods =  Vec::new();
         for line in &reply[1..] {
             match line.split(" ").collect::<Vec<_>>()[0] {
                 "250-AUTH" => {
                     let auth = re_auth.captures(line).unwrap();
-                    let auth_methods = auth.name("auth_methods").unwrap();
+                    auth_methods = auth.name("auth_methods").unwrap().split(",")
+                        .map(|x| match x {
+                            "NULL" => AuthMethod::Null,
+                            "HASHEDPASSWORD" => AuthMethod::HashedPassword,
+                            "COOKIE" => AuthMethod::Cookie,
+                            "SAFECOOKIE" => AuthMethod::SafeCookie,
+                            _ => panic!("Auth method {} not supported", x),
+                        }).collect::<Vec<_>>();
                     let maybe_cookie_files = auth.name("maybe_cookie_files").unwrap();
-                    for caps in re_cookiefile.captures_iter(maybe_cookie_files) {
-                        debug!("Cookie file={}", caps.name("cookie_file").unwrap());
+                    for caps in re_cookie_file.captures_iter(maybe_cookie_files) {
+                        cookie_files.push(caps.name("cookie_file").unwrap().to_string());
                     }
-                    debug!("Auth methods={}", auth_methods);
+                    //debug!("Auth methods={:?}", auth_methods);
+                    //debug!("Cookie files={:?}", cookie_files);
                 },
                 "250-VERSION" => {
                     let ver = re_tor_version.captures(line).unwrap();
-                    let tor_version = ver.name("tor_version").unwrap();
+                    tor_version = ver.name("tor_version").unwrap().to_string();
                     let opt_arguments = ver.name("opt_arguments").unwrap();
-                    debug!("Tor version={}, optional args={}", tor_version, opt_arguments);
+                    //debug!("Tor version={}, optional args={}", tor_version, opt_arguments);
                 },
                 "250" => debug!("OK"),
                 _ => (),
             } 
         }
-        debug!("version = {}", version);
+        //debug!("version = {}", version);
+        ProtocolInfo {
+            protocol_info_ver: version,
+            tor_ver: tor_version,
+            auth_methods: auth_methods,
+            cookie_files: cookie_files,
+        }
     }
 //    fn connect(mut &self) {
 //        match self.connection {
@@ -144,7 +182,8 @@ fn main() {
     info!("Starting Tor Controller!");
     let mut controller = Controller::from_port(9051);
     //controller.assert("PROTOCOLINFO");
-    controller.protocolinfo();
+    let protocolinfo = controller.protocolinfo();
+    debug!("{:?}", protocolinfo);
 //    controller.write("PROTOCOLINFO\r\n");
 //    controller.write("PROTOCOLINFO\r\n");
 }
